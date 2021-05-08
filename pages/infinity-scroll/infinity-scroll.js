2272d78a88c7b0448f9ff8a7008a378e4684b23f
Krachi Crunch <khoii87@gmail.com>
2021-05-08 20:29:30 +0200

    Add: ✨JavaScript logic to display the unsplash images

--- pages/infinity-scroll/infinity-scroll.js
+++ pages/infinity-scroll/infinity-scroll.js
@@ -1,12 +1,51 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('infinity-loader');
+
+let photos = [];
+
 const count = 10;
 const apiKey = 'azDEgfUl7cGcx8ALUFh1hFLFbAvQ8IGwnR29V5lpfXM';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-async function getPhotos(url) {
-  const response = await fetch(url);
-  return await response.json();
-}
+fetch(apiUrl)
+  .then((res) => res.json())
+  .then((photo) => {
+    photo.map((photo) => {
+      const {
+        'photo.links.html': photoLink,
+        'photo.urls.regular': photoUrl,
+        'photo.alt_description': photoDescription,
+      } = photo;
 
-getPhotos(apiUrl)
-  .then((photo) => console.log(photo))
+      const anchor = document.createElement('a');
+
+      setAttributes(anchor, {
+        href: photo.links.html,
+        target: '_blank',
+      });
+
+      //create image photo
+      const img = document.createElement('img');
+      //   img.setAttribute('src', photo.urls.regular);
+      //   img.setAttribute('alt', photo.alt_description);
+      //   img.setAttribute('title', photo.alt_description);
+
+      setAttributes(img, {
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+      });
+
+      // Put <img> inside  <a>, then put both inside imageContainer Element
+
+      anchor.appendChild(img);
+      imageContainer.appendChild(anchor);
+    });
+  })
   .catch((err) => console.log(err));
+
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
