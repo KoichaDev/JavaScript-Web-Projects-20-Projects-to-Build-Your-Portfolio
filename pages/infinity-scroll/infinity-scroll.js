c3a3fdf3f41f3884b9e055229fe72cb08e6ce3f8
Krachi Crunch <khoii87@gmail.com>
2021-05-08 20:38:13 +0200

    Add: ✨ Infinity Scroll Part 1
    This part is getting the infinity photo for sure, but we need to
    be more specific with the loading when we have reached the bottom
    of the scrolling window. When it has reached to the bottom, then
    we want to trigger a new API call to create a sort of "infinity scroll"
    effect

--- pages/infinity-scroll/infinity-scroll.js
+++ pages/infinity-scroll/infinity-scroll.js
@@ -7,43 +7,41 @@ const count = 10;
 const apiKey = 'azDEgfUl7cGcx8ALUFh1hFLFbAvQ8IGwnR29V5lpfXM';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-fetch(apiUrl)
-  .then((res) => res.json())
-  .then((photo) => {
-    photo.map((photo) => {
-      const {
-        'photo.links.html': photoLink,
-        'photo.urls.regular': photoUrl,
-        'photo.alt_description': photoDescription,
-      } = photo;
+window.addEventListener('scroll', () => {
+  const windowHeight = window.innerHeight + window.scrollY;
+  const fullHeightImage = document.body.offsetHeight - 1000;
 
-      const anchor = document.createElement('a');
+  if (windowHeight >= fullHeightImage) {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((photo) => {
+        photo.map((photo) => {
+          const anchor = document.createElement('a');
 
-      setAttributes(anchor, {
-        href: photo.links.html,
-        target: '_blank',
-      });
+          setAttributes(anchor, {
+            href: photo.links.html,
+            target: '_blank',
+          });
 
-      //create image photo
-      const img = document.createElement('img');
-      //   img.setAttribute('src', photo.urls.regular);
-      //   img.setAttribute('alt', photo.alt_description);
-      //   img.setAttribute('title', photo.alt_description);
+          const img = document.createElement('img');
 
-      setAttributes(img, {
-        src: photo.urls.regular,
-        alt: photo.alt_description,
-        title: photo.alt_description,
-      });
+          setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+          });
 
-      // Put <img> inside  <a>, then put both inside imageContainer Element
+          // Put <img> inside  <a>, then put both inside imageContainer Element
 
-      anchor.appendChild(img);
-      imageContainer.appendChild(anchor);
-    });
-  })
-  .catch((err) => console.log(err));
+          anchor.appendChild(img);
+          imageContainer.appendChild(anchor);
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+});
 
+// Set DOM elements attributes
 function setAttributes(element, attributes) {
   for (const key in attributes) {
     element.setAttribute(key, attributes[key]);
