d4b2d8c5399a2587cdf37155764b2f9181105810
Krachi Crunch <khoii87@gmail.com>
2021-05-09 19:57:42 +0200

    Merge: 🔀 from branch 'feature/project/picture-in-picture'
    Add: ✨ Adding Picture in picture functionality to work

---
+++ pages/picture-in-picture/picture-in-picture.js
@@ -0,0 +1,32 @@
+const videoElement = document.getElementById('picture-video');
+const button = document.getElementById('picture-button');
+
+// Prompt user to select media stream, and pass to the video element, then play
+
+async function selectMediaStream() {
+  try {
+    // Waiting to assign the video media stream when the user has selected which screen or window they want to share
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+
+    // We pass the mediaStream to the videoElement of the src attribute
+    videoElement.srcObject = mediaStream;
+
+    // When video has loaded the meta Data, then it's going to call the function to play the video
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (err) {
+    console.log('selectMediaStream() error:', error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  button.disabled = true;
+
+  // Start Picture-in-picture
+  await videoElement.requestPictureInPicture();
+
+  // Reset Button will happen if we successfully triggering the requestPictureInPicture();
+  button.disabled = false;
+});
+selectMediaStream();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './scss/main.scss';
 import '../pages/infinity-scroll/infinity-scroll';
+import '../pages/picture-in-picture/picture-in-picture';
 import '../pages/quote-generator/quote-generator';
