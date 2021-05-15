49007f516ac723e0ae14422c201faedb18d6929a
Krachi Crunch <khoii87@gmail.com>
2021-05-16 01:09:48 +0200

    Add: ✨/💄 Toggling Dark/light mode

--- pages/dark-light-mode/dark-light-mode.js
+++ pages/dark-light-mode/dark-light-mode.js
@@ -4,7 +4,15 @@ if (currentSite) {
   const toggleSwitch = document.getElementById('toggle-dark-light-mode');
 
   function switchTheme(e) {
-    console.log(e);
+    const toggle = e.target.checked;
+
+    // Document.documentElement returns the Element that is the root element of the document (for example, the <html> element for HTML documents).
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement
+    if (toggle) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
   }
 
   toggleSwitch.addEventListener('change', switchTheme);
