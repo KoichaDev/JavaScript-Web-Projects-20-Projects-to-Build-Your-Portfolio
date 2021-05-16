79224dcf09d10e7a2d52117c5f7e2e7777eebcb3
Krachi Crunch <khoii87@gmail.com>
2021-05-16 22:03:42 +0200

    Add: ✨ AOS animated library
    Source:
    https://michalsnik.github.io/aos/

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './scss/main.scss';
+import '../pages/animated-template/animated-template';
 import '../pages/dark-light-mode/dark-light-mode';
 import '../pages/infinity-scroll/infinity-scroll';
 import '../pages/joke-teller/joke-teller';
