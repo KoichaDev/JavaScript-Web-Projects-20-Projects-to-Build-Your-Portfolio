eb7626be8c3118a567aaca44b02002f45f4018a5
Krachi Crunch <khoii87@gmail.com>
2021-05-20 00:19:13 +0200

    Refactor: ♻️ The code should be more cleaner and shorter compared to the old version

--- pages/navigation-nation/navigation-nation.js
+++ pages/navigation-nation/navigation-nation.js
@@ -1,6 +1,8 @@
 const currentSite = window.location.href.split('/').includes('navigation-nation.html');
+const lastURLParameter = window.location.href.split('/');
+lastURLParameter[lastURLParameter.length - 1];
 
-if (currentSite) {
+if (currentSite || lastURLParameter) {
   const menuBars = document.getElementById('menu-bars');
   const overlay = document.getElementById('overlay');
   const navOne = document.getElementById('nav-1');
@@ -8,53 +10,57 @@ if (currentSite) {
   const navThree = document.getElementById('nav-3');
   const navFour = document.getElementById('nav-4');
   const navFive = document.getElementById('nav-5');
+  const navItems = [navOne, navTwo, navThree, navFour, navFive];
 
   function toggleNav() {
     // Toggle Menu Bars opened/closed
-    menuBars.classList.toggle('change');
+    toggleClassName(menuBars, 'change');
 
     // Toggle Menu active
-    overlay.classList.toggle('overlay-active');
+    toggleClassName(overlay, 'overlay-active');
 
-    if (overlay.classList.contains('overlay-active')) {
+    if (containClassName(overlay, 'overlay-active')) {
       // Animate In - Overlay
-      overlay.classList.add('overlay-slide-right');
-      overlay.classList.remove('overlay-slide-left');
+      replaceClassNameAnimation(overlay, 'overlay-slide-left', 'overlay-slide-right');
 
       // Animate In - Nav Items
-      navOne.classList.remove('slide-out-1');
-      navOne.classList.add('slide-in-1');
-      navTwo.classList.remove('slide-out-2');
-      navTwo.classList.add('slide-in-2');
-      navThree.classList.remove('slide-out-3');
-      navThree.classList.add('slide-in-3');
-      navFour.classList.remove('slide-out-4');
-      navFour.classList.add('slide-in-4');
-      navFive.classList.remove('slide-out-5');
-      navFive.classList.add('slide-in-5');
+      setNavAnimationDirection('out', 'in');
     } else {
       // Animate Out - Overlay
-      overlay.classList.add('overlay-slide-left');
-      overlay.classList.remove('overlay-slide-right');
+      replaceClassNameAnimation(overlay, 'overlay-slide-right', 'overlay-slide-left');
 
       // Animate Out - Nav Items
-      navOne.classList.remove('slide-in-1');
-      navOne.classList.add('slide-out-1');
-      navTwo.classList.remove('slide-in-2');
-      navTwo.classList.add('slide-out-2');
-      navThree.classList.remove('slide-in-3');
-      navThree.classList.add('slide-out-3');
-      navFour.classList.remove('slide-in-4');
-      navFour.classList.add('slide-out-4');
-      navFive.classList.remove('slide-in-5');
-      navFive.classList.add('slide-out-5');
+      setNavAnimationDirection('in', 'out');
+    }
+  }
+
+  // Toggling the className for the specific element
+  function toggleClassName(element, className) {
+    element.classList.toggle(className);
+  }
+
+  // Return true or false
+  function containClassName(element, className) {
+    return element.classList.contains(className);
+  }
+
+  // Use the element to exchange class Name
+  function replaceClassNameAnimation(element, classNameOne, classNameTwo) {
+    return element.classList.replace(classNameOne, classNameTwo);
+  }
+
+  // Control Navigation Animation
+  function setNavAnimationDirection(directionOne, directionTwo) {
+    let increment = 1;
+    for (const navItem of navItems) {
+      const count = increment++;
+      navItem.classList.replace(`slide-${directionOne}-${count}`, `slide-${directionTwo}-${count}`);
     }
   }
 
   menuBars.addEventListener('click', toggleNav);
-  navOne.addEventListener('click', toggleNav);
-  navTwo.addEventListener('click', toggleNav);
-  navThree.addEventListener('click', toggleNav);
-  navFour.addEventListener('click', toggleNav);
-  navFive.addEventListener('click', toggleNav);
+
+  for (navItem of navItems) {
+    navItem.addEventListener('click', toggleNav);
+  }
 }
