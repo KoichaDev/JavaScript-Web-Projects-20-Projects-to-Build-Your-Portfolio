561224651766ffe243de9327ff2bbfeb16623a52
Krachi Crunch <khoii87@gmail.com>
2021-05-19 20:57:51 +0200

    Add: ✨ Setting up HTML and SCSS

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import '../pages/animated-template/animated-template';
 import '../pages/dark-light-mode/dark-light-mode';
 import '../pages/infinity-scroll/infinity-scroll';
 import '../pages/joke-teller/joke-teller';
+import '../pages/navigation-nation/navigation-nation';
 import '../pages/picture-in-picture/picture-in-picture';
 import '../pages/quote-generator/quote-generator';
