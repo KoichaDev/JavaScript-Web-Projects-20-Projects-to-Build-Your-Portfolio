af8dfa029a87b8614cfe5030c8d9bbecb05be660
Krachi Crunch <khoii87@gmail.com>
2021-05-19 23:39:43 +0200

    Add: ✨/💄 Animation nav menu for fade-in and fade-out

--- pages/navigation-nation/navigation-nation.js
+++ pages/navigation-nation/navigation-nation.js
@@ -20,10 +20,34 @@ if (currentSite) {
       // Animate In - Overlay
       overlay.classList.add('overlay-slide-right');
       overlay.classList.remove('overlay-slide-left');
+
+      // Animate In - Nav Items
+      navOne.classList.remove('slide-out-1');
+      navOne.classList.add('slide-in-1');
+      navTwo.classList.remove('slide-out-2');
+      navTwo.classList.add('slide-in-2');
+      navThree.classList.remove('slide-out-3');
+      navThree.classList.add('slide-in-3');
+      navFour.classList.remove('slide-out-4');
+      navFour.classList.add('slide-in-4');
+      navFive.classList.remove('slide-out-5');
+      navFive.classList.add('slide-in-5');
     } else {
       // Animate Out - Overlay
       overlay.classList.add('overlay-slide-left');
       overlay.classList.remove('overlay-slide-right');
+
+      // Animate Out - Nav Items
+      navOne.classList.remove('slide-in-1');
+      navOne.classList.add('slide-out-1');
+      navTwo.classList.remove('slide-in-2');
+      navTwo.classList.add('slide-out-2');
+      navThree.classList.remove('slide-in-3');
+      navThree.classList.add('slide-out-3');
+      navFour.classList.remove('slide-in-4');
+      navFour.classList.add('slide-out-4');
+      navFive.classList.remove('slide-in-5');
+      navFive.classList.add('slide-out-5');
     }
   }
 
